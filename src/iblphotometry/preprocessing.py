3cd1e5bfb555712f5140a5d87ab135b6ea53b097
owinter <olivier.winter@hotmail.fr>
2024-06-26 14:56:29 +0100

    add a couple of pre-processing methods to try

--- src/iblphotometry/preprocessing.py
+++ src/iblphotometry/preprocessing.py
@@ -10,36 +10,79 @@ import ibldsp.utils
 from iblutil.numerical import rcoeff
 
 
-def photobleaching_lowpass(raw_calcium, **params):
+def preprocess_sliding_mad(raw_calcium, times, fs=None, wlen=120, overlap=90, returns_gain=False, **params):
+    """
+    Applies one pass of fiber photobleaching
+    :param raw_calcium:
+    :param times:
+    :param fs:
+    :param wlen:
+    :param overlap:
+    :param params:
+    :return:
+    """
+    calcium = photobleaching_lowpass(raw_calcium, fs, **params)
+    wg = ibldsp.utils.WindowGenerator(ns=calcium.size, nswin=int(wlen * fs), overlap=overlap)
+    trms = np.array([first for first, last in wg.firstlast]) / fs
+    rmswin = psth(calcium, times, t_events=trms, fs=fs, peri_event_window=[0, wlen])
+    gain = np.median(np.abs(calcium)) / np.median(np.abs(rmswin), axis=0)
+    gain = np.interp(times, trms, gain)
+    if returns_gain:
+        return calcium * gain, gain
+    else:
+        return calcium * gain
+
+
+def jove2019(raw_calcium, raw_isosbestic, fs, **params):
+    """
+    Martianova, Ekaterina, Sage Aronson, and Christophe D. Proulx. "Multi-fiber photometry to record neural activity in freely-moving animals." JoVE (Journal of Visualized Experiments) 152 (2019): e60278.
+    :param raw_calcium:
+    :param raw_isosbestic:
+    :param params:
+    :return:
+    """
+    # the first step is to remove the photobleaching w
+    sos = scipy.signal.butter(fs=fs, output='sos', **params.get('butterworth_lowpass', {'N': 3, 'Wn': 0.01, 'btype': 'lowpass'}))
+    calcium = raw_calcium - scipy.signal.sosfiltfilt(sos, raw_calcium)
+    isosbestic = raw_isosbestic - scipy.signal.sosfiltfilt(sos, raw_isosbestic)
+    calcium = (calcium - np.median(calcium)) / np.std(calcium)
+    isosbestic = (isosbestic - np.median(isosbestic)) / np.std(isosbestic)
+    m = np.polyfit(isosbestic, calcium, 1)
+    ref = isosbestic * m[0] + m[1]
+    ph = (calcium - ref) / 100
+    return ph
+
+
+def photobleaching_lowpass(raw_calcium, fs, **params):
     """
     Here the isosbestic recording is ignored, and the reference is computed as the low-pass component of the
     calcium band signal.
     :param calcium:
     :param params: dictionary with parameters
-        butterworth_reference: dictionary with parameters for the butterworth filter applied to the
-        calcium band for the sole purpose of regression {'N': 3, 'Wn': 0.01, 'btype': 'lowpass'}
+    {
+        'butterworth_lowpass': {'N': 3, 'Wn': 0.01, 'btype': 'lowpass'}
+        }
+        dictionary with parameters for the butterworth filter applied to the calcium band for the sole purpose of regression
     :return:
     iso (np.array): the corrected isosbestic signal to be used as control
     ph (np.array): the corrected calcium signal
     """
     params = {} if params is None else params
-
-    sos = scipy.signal.butter(**params.get('butterworth_reference', {'N': 3, 'Wn': 0.01, 'btype': 'lowpass'}), output='sos')
+    sos = scipy.signal.butter(fs=fs, output='sos', **params.get('butterworth_lowpass', {'N': 3, 'Wn': 0.01, 'btype': 'lowpass'}))
     calcium_lp = scipy.signal.sosfiltfilt(sos, raw_calcium)
     calcium = (raw_calcium - calcium_lp) / calcium_lp
     return calcium
 
 
-def isosbestic_regression(isosbestic, calcium, fs, **params):
+def isosbestic_regression(raw_isosbestic, raw_calcium, fs, **params):
     """
     Prototype of baseline correction for photometry data.
     Fits a low pass version of the isosbestic signal to the calcium signal. The baseline signal is
     the low pass isosbestic signal multiplied by the fit slope and added to the fit intercept.
     The corrected signal is the calcium signal minus the baseline signal divided by the baseline signal.
     We apply the same procedure to the full-band isosbestic signal to check for remaining correlations.
-    :param isosbestic:
-    :param calcium:
-    :param calcium:
+    :param raw_isosbestic:
+    :param raw_calcium:
     :param params: dictionary with parameters
         butterworth_regression: dictionary with parameters for the butterworth filter applied to both isosbestic and
         calcium band for the sole purpose of regression {'N': 3, 'Wn': 0.01, 'btype': 'lowpass'}
@@ -52,12 +95,12 @@ def isosbestic_regression(isosbestic, calcium, fs, **params):
     params = {} if params is None else params
 
     sos = scipy.signal.butter(**params.get('butterworth_regression', {'N': 3, 'Wn': 0.1, 'btype': 'lowpass', 'fs': fs}), output='sos')
-    calcium_lp = scipy.signal.sosfiltfilt(sos, calcium)
-    isosbestic_lp = scipy.signal.sosfiltfilt(sos, isosbestic)
+    calcium_lp = scipy.signal.sosfiltfilt(sos, raw_calcium)
+    isosbestic_lp = scipy.signal.sosfiltfilt(sos, raw_isosbestic)
     m = np.polyfit(isosbestic_lp, calcium_lp, 1)
 
     ref = isosbestic_lp * m[0] + m[1]
-    ph = (calcium - ref) / ref
+    ph = (raw_calcium - ref) / ref
 
     butterworth_signal = params.get('butterworth_signal', {'N': 3, 'Wn': 10, 'btype': 'lowpass', 'fs': fs})
     if butterworth_signal is not None:
@@ -82,7 +125,7 @@ def isosbestic_correction_dataframe(df_photometry):
     return df_photometry
 
 
-def psth(calcium, times, t_events, fs, peri_event_window=None):
+def psth(calcium, times, t_events, fs=None, peri_event_window=None):
     """
     Compute the peri-event time histogram of a calcium signal
     :param calcium:
@@ -92,17 +135,21 @@ def psth(calcium, times, t_events, fs, peri_event_window=None):
     :param peri_event_window:
     :return:
     """
+    fs = 1 / np.median(np.diff(times)) if fs is None else fs
     peri_event_window = [-1, 2] if peri_event_window is None else peri_event_window
     # compute a vector of indices corresponding to the perievent window at the given sampling rate
-    sample_window = np.arange(peri_event_window[0] * fs, peri_event_window[1] * fs + 1)
+    sample_window = np.arange(peri_event_window[0] * fs, peri_event_window[1] * fs + 1).astype(int)
     # we inflate this vector to a 2d array where each column corresponds to an event
     idx_psth = np.tile(sample_window[:, np.newaxis], (1, t_events.size))
     # we add the index of each event too their respective column
     idx_event = np.searchsorted(times, t_events)
     idx_psth += idx_event
+    i_out_of_bounds = np.logical_or(idx_psth > (calcium.size - 1), idx_psth < 0)
+    idx_psth[i_out_of_bounds] = -1
     psth = calcium[idx_psth]  # psth is a 2d array (ntimes, nevents)
-    psth[idx_psth > (calcium.size - 1)] = np.nan  # remove events that are out of bounds
+    psth[i_out_of_bounds] = np.nan  # remove events that are out of bounds
     return psth
+
 
 def sliding_rcoeff(signal_a, signal_b, nswin, overlap=0):
     """
---
+++ src/tests/integration.py
@@ -0,0 +1,78 @@
+
+
+
+from pathlib import Path
+
+
+
+
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import iblphotometry.plots
+import iblphotometry.preprocessing
+
+path_integration_data = Path("/home/olivier/Documents/photometry/integration")
+path_figures = path_integration_data.joinpath('figures')
+
+
+for parquet_file in path_integration_data.glob('*.pqt'):
+    df_photometry = pd.read_parquet(parquet_file)
+    raw_calcium = df_photometry['raw_calcium'].values
+    raw_isosbestic = df_photometry['raw_isosbestic'].values
+    times = df_photometry['times'].values
+    fs = 1 / np.median(np.diff(times))
+    suptitle = parquet_file.stem
+
+    # plot raw data
+    iblphotometry.plots.plot_raw_data_df(
+        df_photometry,
+        suptitle=suptitle,
+        output_file=path_figures.joinpath(f"{parquet_file.stem}_00.png")
+    )
+
+    # photobleach
+    calcium = iblphotometry.preprocessing.photobleaching_lowpass(raw_calcium, fs=fs)
+    isosbestic_control = iblphotometry.preprocessing.photobleaching_lowpass(raw_isosbestic, fs=fs)
+    iblphotometry.plots.plot_photometry_traces(
+        times,
+        isosbestic_control,
+        calcium,
+        suptitle=parquet_file.stem,
+        output_file=path_figures.joinpath(f"{parquet_file.stem}_01_photobleach.png")
+    )
+
+    # jove_2019
+    calcium = iblphotometry.preprocessing.jove2019(raw_calcium, raw_isosbestic, fs=fs)
+    isosbestic_control = iblphotometry.preprocessing.jove2019(raw_isosbestic, raw_isosbestic, fs=fs)
+    iblphotometry.plots.plot_photometry_traces(
+        times,
+        isosbestic_control,
+        calcium,
+        suptitle=parquet_file.stem,
+        output_file=path_figures.joinpath(f"{parquet_file.stem}_02_jove2019.png")
+    )
+
+    # sliding mad
+    calcium = iblphotometry.preprocessing.preprocess_sliding_mad(raw_calcium, times, fs=fs)
+    isosbestic_control = iblphotometry.preprocessing.preprocess_sliding_mad(raw_isosbestic, times, fs=fs)
+    iblphotometry.plots.plot_photometry_traces(
+        times,
+        isosbestic_control,
+        calcium,
+        suptitle=parquet_file.stem,
+        output_file=path_figures.joinpath(f"{parquet_file.stem}_03_sliding_mad.png")
+    )
+
+
+    print(parquet_file)
+    # if suptitle == 'ZFM-04022_2023-01-12_003_alf_Region3G':
+    #     break
+    # break
+    plt.close('all')
+
+
+
