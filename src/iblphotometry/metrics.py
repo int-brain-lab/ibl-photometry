b78b07baa2dc36d0b7343ce603af807ad9398016
GaelleChapuis <gaelle.chapuis.ibl@gmail.com>
2024-10-08 17:03:28 +0200

    QC metric ttest

--- src/iblphotometry/metrics.py
+++ src/iblphotometry/metrics.py
@@ -3,6 +3,8 @@ import pynapple as nap
 from scipy.stats import linregress, skew
 from utils import make_sliding_window, z
 import outlier_detection
+import scipy.stats as stats
+from iblphotometry.preprocessing import psth
 
 
 # metrics: - common definition: all receive a np.array as first argument, all other must be keyword arguments
@@ -54,6 +56,29 @@ def n_unique_samples(A: np.array):
 
 def n_spikes(A: np.array, sd: int):
     return outlier_detection.detect_spikes(A, sd=sd).shape[0]
+
+
+def ttest_pre_post(calcium, times, t_events, fs, pre_w=[-1, -0.2], post_w=[0.2, 1], confid=0.001):
+    """
+    :param calcium: np array, trace of the signal to be used
+    :param times: np array, times of the signal to be used
+    :param t_events: np array, times of the events to align to
+    :param fs: float, sampling frequency of the signal
+    :param pre_w: list of floats, pre window sizes in seconds
+    :param post_w: list of floats, post window sizes in seconds
+    :param confid: float, confidence level (alpha)
+    :return: boolean, True if metric passes
+    """
+    psth_pre = psth(calcium, times, t_events, fs=fs, peri_event_window=pre_w)[0]
+    psth_post = psth(calcium, times, t_events, fs=fs, peri_event_window=post_w)[0]
+
+    # Take median value of signal over time
+    pre = np.median(psth_pre, axis=0)
+    post = np.median(psth_post, axis=0)
+    # Paired t-test
+    ttest = stats.ttest_rel(pre, post)
+    passed_confg = ttest.pvalue < confid
+    return passed_confg
 
 
 def n_outliers(A: np.array, w_size: int = 1000, alpha: float = 0.0005):
