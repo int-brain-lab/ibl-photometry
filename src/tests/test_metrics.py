d40a158400512c39ce2da127bba429d1d3edd6dc
georg <grg2rsr@gmail.com>
2024-11-29 18:13:48 +0000

    metrics type checking added, tests added (to be expanded)

--- src/tests/test_metrics.py
+++ src/tests/test_metrics.py
@@ -1,39 +1,64 @@
-# import pandas as pd
-# from pathlib import Path
-# from one.api import ONE
-# from iblphotometry.preprocessing import jove2019
-# from iblphotometry.metrics import ttest_pre_post
+from pathlib import Path
+import unittest
+import iblphotometry.io as fio
+import iblphotometry.metrics as metrics
+import pandas as pd
+
+data_folder = Path(__file__).parent / 'data'
+
+# TODO make this a list and have a few
+# this is currently ony alejandro
+session_folder = Path('wittenlab/Subjects/fip_40/2023-05-18/001')
+signal_pqt_path_rel = Path('alf/photometry/photometry.signal.pqt')
+photometryROI_locations_pqt_path_rel = Path(
+    'alf/photometry/photometryROI.locations.pqt'
+)
+raw_neurophotometrics_file_path_rel = Path(
+    'raw_photometry_data/_neurophotometrics_fpData.raw.pqt'
+)
+
+signal_pqt_path = data_folder / session_folder / signal_pqt_path_rel
+photometryROI_locations_pqt_path = (
+    data_folder / session_folder / photometryROI_locations_pqt_path_rel
+)
+raw_neurophotometrics_file_path = (
+    data_folder / session_folder / raw_neurophotometrics_file_path_rel
+)
+
+trials_table_file_path = data_folder / session_folder / 'alf/_ibl_trials.table.pqt'
 
 
-# def test_ttest_pre_post():
-#     # Get data
-#     one = ONE()
-#     eid = '77a6741c-81cc-475f-9454-a9b997be02a4'
+class TestMetrics(unittest.TestCase):
+    # think here about the possible use cases
 
-#     # Load NP file locally - TODO: this is local directory to Github
-#     # nph_path = Path(f'../src/tests/data/{eid}')
-#     nph_path = Path(f'/Users/gaellechapuis/Desktop/FiberPhotometry/{eid}')
-#     df_nph = pd.read_parquet(nph_path.joinpath(f'raw_photometry.pqt'))
+    def test_metrics(self):
+        # get data
+        raw_tfs = fio.from_pqt(signal_pqt_path)
+        trials = pd.read_parquet(trials_table_file_path)
 
-#     # Load trial from ONE
-#     a = one.load_object(eid, 'trials')
-#     df_trials = a.to_df()
+        signal_bands = list(raw_tfs.keys())
+        raw_tf = raw_tfs[signal_bands[0]]
+        raw_tsd = raw_tf[:, 0]
 
-#     # Ugly way to get sampling frequency
-#     time_diffs = df_nph["times"].diff().dropna()
-#     fs = 1 / time_diffs.median()
+        # testing metrics with nap.Tsd
+        # metrics.bleaching_tau(raw_tsd)
+        metrics.n_spikes(raw_tsd)
+        metrics.detect_spikes(raw_tsd)
+        metrics.n_outliers(raw_tsd)
+        metrics.n_unique_samples(raw_tsd)
+        metrics.signal_asymmetry(raw_tsd)
+        metrics.signal_skew(raw_tsd)
+        metrics.percentile_dist(raw_tsd)
 
-#     # Process signal
-#     df_nph['calcium_jove2019'] = jove2019(df_nph["raw_calcium"], df_nph["raw_isosbestic"], fs=fs)
-
-#     # Get event
-#     event = 'feedback_times'
-#     calcium = df_nph['calcium_jove2019'].values
-#     times = df_nph['times'].values
-#     t_events = df_trials[event]
-
-#     pre_w = [-1, -0.2]  # seconds
-#     post_w = [0.2, 1]  # seconds
-
-#     pass_test = ttest_pre_post(calcium, times, t_events, fs, pre_w=pre_w, post_w=post_w)
-#     assert pass_test == True
+        BEHAV_EVENTS = [
+            # 'stimOn_times',
+            # 'goCue_times',
+            # 'response_times',
+            'feedback_times',
+            # 'firstMovement_times',
+            # 'intervals_0',
+            # 'intervals_1',
+        ]
+        for event_name in BEHAV_EVENTS:
+            metrics.ttest_pre_post(raw_tf, trials, event_name)
+            metrics.has_responses(raw_tsd, trials, BEHAV_EVENTS)
