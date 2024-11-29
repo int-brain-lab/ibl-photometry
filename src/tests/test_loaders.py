6eab30476255eef4b9fb6b05870da575e525ce90
georg <grg2rsr@gmail.com>
2024-11-29 12:34:58 +0000

    adding raw neurophotometrics reader with validation by default, numpy io added, (attempt #2)

--- src/tests/test_loaders.py
+++ src/tests/test_loaders.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import unittest
 import iblphotometry.io as fio
+import numpy as np
 
 data_folder = Path(__file__).parent / 'data'
 
@@ -28,7 +29,14 @@ class TestLoaders(unittest.TestCase):
     # think here about the possible use cases
 
     # to read from a .csv from disk
-    #
+    def test_from_array(self):
+        n_samples = 1000
+        n_channels = 3
+        times = np.linspace(0, 100, n_samples)
+        data = np.random.randn(n_samples, n_channels)
+        names = ['a', 'b', 'c']
+        fio.from_array(times, data, names)
+
     def test_from_csv(self):
         # TODO get files
         ...
@@ -40,7 +48,8 @@ class TestLoaders(unittest.TestCase):
 
     # for neurophotometrics hardware
     def test_from_raw_neurophotometrics_output(self):
-        fio.from_raw_neurophotometrics(raw_neurophotometrics_file_path)
+        neurophotometrics_csv_path = data_folder / 'raw_photometry.csv'
+        fio.from_raw_neurophotometrics(neurophotometrics_csv_path)
 
     # from pqt files as they are returned from ONE by .load_dataset()
     def test_from_pqt(self):
